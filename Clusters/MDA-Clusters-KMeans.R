c28caa7ee8c04e003e00433b740df54f9336099b
Juan Esteban Mejía Velásquez <juanesteban8@gmail.com>
2016-03-28 22:13:43 -0500

    28/03/2016

--- Clusters/MDA-Clusters-KMeans.R
+++ Clusters/MDA-Clusters-KMeans.R
@@ -1,20 +1,23 @@
 
 '''
-Minería de datos apicada
-K Means
+Minería de Datos Aplicada
+Universidad Nacional de Colombia
+
+Ilustración K-means
+
 Juan Esteban Mejía Velásquez
 
 '''
-# Cargar el conjunto de datos de iris 
 
-Iris <- iris
 
-# Mostrar a estructura de conjunto de datos}
 
-str(Iris)
-attach(Iris) # SEparar as variabes de data.frame original
+## Mostrar a estructura de conjunto de datos}
 
-# Un poco de exploracion : 
+str(iris)
+attach(iris) # Separar as variabes de data.frame original 
+             # (poner iris en el search path)
+
+## Un poco de exploracion : 
   
  # Graficos de dispersión 
 
@@ -35,41 +38,40 @@ plot(Sepal.Length, Sepal.Width,col=Species,pch=as.numeric(Species))
     parcoord(Iris[1:4], col=iris$Species,var.label=T)
 
 
-# Contruccon de un modeo estandar de k-meands
-set.seed(123)
+## Contruccon de un modeo estandar de k-meands
+    
+set.seed(123) # la semilla inicial es para que nos de los mismo resultados
+              # Recurde que los clústers son sensibles a los valores iniciales
+
 KM.Iris<-kmeans(Iris[1:4], 3,iter.max=1000,
                 algorithm = c("Lloyd") )
 
-# Obtencion de mas informacion acerca de modelo
-# Tamaño de los custers
+## Obtencion de mas informacion acerca de modelo
+ # Tamaño de los custers
 
 KM.Iris$size
 
-# Centro de los cluster por variable
+ # Centro de los cluster por variable
 
 KM.Iris$centers
 
-# Table tabla con el numero de record por especie
+#  tabla con el numero de record por especie
 
 table(Iris$Species,KM.Iris$cluster)
 
-# Traducir a dos dimensiones usando
-# escaamiento mutidimensional
+## Traducir a dos dimensiones usando escalamiento mutidimensional
 
 Iris.dist <- dist(Iris[1:4])
 Iris.mds <- cmdscale(Iris.dist)
 
 # Graficar los punto en el espacio de dos dimensiones
-# Abrir array para la graficacion
 
 par(mfrow = c(1, 2))
 
-# Cargar e instar el paquete de  scatterplot3d
 
-suppressWarnings(
-  suppressMessages(if
-                   (!require(scatterplot3d, quietly=TRUE))
-    install.packages("scatterplot3d")))
+# Cargar e instalar el paquete de  scatterplot3d para graficos 3D
+
+if(!require(scatterplot3d, quietly=TRUE))install.packages("scatterplot3d")
 
 library("scatterplot3d")
 
@@ -84,10 +86,9 @@ g3d$points3d(Iris.mds,col=KM.Iris$cluster,pch=chars)
 
 # Graficar en 2d
 
-plot(Iris.mds, col = KM.Iris$cluster, pch = chars, xlab = "Indeice", ylab = "Y")
+plot(Iris.mds, col = KM.Iris$cluster, pch = chars, xlab = "indice", ylab = "Y")
 
-# Adiccionar la variabe "custer" a
-# el conjunto de datos original
+# Adiccionar la variabe "custer" a el conjunto de datos original
 
 Iris.Cluster<-cbind(Iris,KM.Iris[1])
 
@@ -107,6 +108,9 @@ for (k in 1:30) {
   SSE[k] = grupos$tot.withinss
 }
 
+par(mfrow=c(1, 1))
+
+
 #
 
 plot(SSE, col = "red", type = "b")
@@ -119,7 +123,7 @@ text(4, 60, "3 Clusters", col = "black", adj = c(0, -0.1), cex = 0.7)
 
 ## Eección de algoritmo
 
-# setiatr los vectores para almacenar os resutados
+# setiar los vectores para almacenar os resutados
 
 Lloyd <- 0
 MacQueen <- 0
@@ -148,26 +152,30 @@ names(Results) <- c("Metodo", "Intermediación")
 
 Resultados
 
-##Otros graficos
+## Otros graficos para la validación del clúster
 
+## La técnica PCA (análisis de componentes principales) lo que 
+## hace es resumir la información de todas las variables en unas 
+## pocas nuevas llamadas componentes. Cada componente explica cierto 
+## porcentaje de la variabilidad total. La función cusplot es una
+## manera de identificar la efectividad de los cluster. Clústeres 
+## bien formados están separados en el plano principal formado por 
+## los componentes 1 y 2. Por el contrario si se ven solapamientos en 
+## el plano principal es porque los clústeres no están bien formados.
 
-suppressWarnings(
-  suppressMessages(if
-                   (!require(cluster, quietly=TRUE))
-    install.packages("cluster")))
+if (!require(cluster, quietly=TRUE)) install.packages("cluster")
 
 
 clusplot(Iris[1:4], KM$cluster, color=TRUE, shade=TRUE,
-         labels=2, lines=1,main='Anaisis de clúster para Iris')
+         labels=2, lines=1,main='Análisis de clúster para Iris')
 
 
 
-#Coeficiente de silueta
+## Coeficiente de silueta
 
-suppressWarnings(suppressMessages(if (!require(HSAUR,
-                                               quietly = TRUE)) install.packages("HSAUR")))
-suppressWarnings(suppressMessages(if (!require(cluster,
-                                               quietly = TRUE)) install.packages("cluster")))
+if (!require(HSAUR,quietly = TRUE)) install.packages("HSAUR")
+if (!require(cluster,quietly = TRUE)) install.packages("cluster")
+
 library("HSAUR")
 library("cluster")
 
@@ -179,12 +187,24 @@ KM <- kmeans(Iris[1:4], 3, iter.max = 1000, algorithm = "Hartigan-Wong")
 
 diss <- daisy(Iris[1:4])
 dE2 <- diss^2
+
+# Creacion de mapa de calor para intuir validez
+
+if (!require(d3heatmap, quietly=TRUE)) install.packages("d3heatmap")
+
+
+library(d3heatmap) # Libreria para heat map interactivos
+
 heatmap(as.matrix(diss))
+
+d3heatmap(diss,  dendrogram = "none")
+
+
 
 # Calculo de silueta
 
 Sil <- silhouette(KM$cl, diss)
-silh
+
 
 # Grarfico
 
@@ -192,6 +212,8 @@ par(mfrow=c(1, 1))
 
 plot(Sil, col = c("red", "green", "blue"))
 
+## Usas la libreria NbClust para determinar e # de cluster mediante 30
+## indices
 
 library(NbClust)
 
@@ -200,6 +222,8 @@ nb <- NbClust(scale(Iris[, 1:4]), distance = "euclidean", min.nc = 2,
 
 
 table(names(nb$Best.nc[1,]), nb$Best.nc[1,])
+
+par(mfrow=c(1, 1))
 
 barplot(table(nb$Best.nc[1,]), border="blue", 
         density=c(90, 70, 50, 40, 30, 20, 10))
