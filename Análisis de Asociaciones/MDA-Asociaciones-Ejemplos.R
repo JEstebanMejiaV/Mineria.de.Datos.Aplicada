b25f5fc246f34c8bdac5f6fe15612d72437b24da
Juan Esteban Mejía Velásquez <juanesteban8@gmail.com>
2017-02-01 10:34:20 -0500

    01/02/2017

--- Análisis de Asociaciones/MDA-Asociaciones-Ejemplos.R
+++ Análisis de Asociaciones/MDA-Asociaciones-Ejemplos.R
@@ -1,15 +1,13 @@
 
-'''
-Minería de Datos Aplicada
-Universidad Nacional de Colombia
 
-Ilustración Reglas de Asociación
+# Minería de Datos Aplicada
+# Universidad Nacional de Colombia
+# 
+# Ilustración Reglas de Asociación
+# 
+# Juan Esteban Mejía Velásquez
 
-Juan Esteban Mejía Velásquez
 
-'''
-
-class(Groceries)
 
 ## Crear como data.frame
 
@@ -24,26 +22,42 @@ library(arulesViz)  # Vizuaización de Reglas de Asociacion
 library(RColorBrewer)  # Paletas de colores para las graficas
 library(cluster)  # Analissi de cluster
 
-data(Groceries)  # Trasnsacciones de tienda desde e paquete arules 
-
+data(Groceries)  # Trasnsacciones de tienda desde el paquete arules 
 
 
 Groceries
 class(Groceries)
 
+## Extraer el númeor de items en la la estructura transaciones 
+## y convertirla en un dataframe
+
 items<-as.data.frame(itemLabels(Groceries))
 colnames(items) <- "Item"
 head(items,10)
+nrow(items) # Número de items (Articulos)
 
 ## Calcular las cuentas de soporte (Suoporte absoluto)
 
 itemFrequency(Groceries,type = "absolute")
 
+head(itemFrequency(Groceries,type = "absolute"), 5)
+
+tail(itemFrequency(Groceries,type = "absolute"))
+
+
+head(sort(itemFrequency(Groceries,type = "absolute"), decreasing = T), 5)
+
 ## Calcular el soporte (Suoporte relativo)
 
-round(itemFrequency(Groceries,type = "relative")*100,2)
 
-## Graficas el soporte
+head(itemFrequency(Groceries,type = "relative"), 10)
+
+round(itemFrequency(Groceries,type = "relative")*100,2) # En porcentaje
+
+head(sort(round(itemFrequency(Groceries,type = "relative")*100,2), decreasing = T), 5)
+
+
+## Graficas el soporte simple
 
 plot(round(itemFrequency(Groceries,type = "relative")*100,2), col="navy")
 
@@ -61,27 +75,52 @@ itemFrequencyPlot(Groceries, support = 0.025, cex.names=0.8, xlim = c(0,0.3),
                   xlab = paste("Proporción de la canasta de bienes que contiene el item",
                                "\n(Frecuencia relativa o Soporte)"))
 
-## Creacion de clúster jerásquico para seguir explorando
+## Creacion de cluster jerásquico para seguir explorando
 
 x <- dissimilarity(Groceries, which = "items")
 x[is.na(x)] <- 1 
 
-hcd<-hclust(x)
-plot(hcd, cex=.6) # cex controla e tamaño de las etiquetas
+HC<-hclust(x)
+plot(HC, cex=.65) # cex controla e tamaño de las etiquetas
 
-## Ahora sí, vamso a aplicar el algorítmo Apriori
+## Ahora , vamos a aplicar el algorótmo Apriori
 
-rules <- apriori (Groceries, parameter = list(supp = 0.001, conf = 0.8))
+# Dada la grna variedad de articulos los soportes son muy bajos, por ello
+# en este caso particular nuestros maores son del orden de 0.001.
 
-rules2 <- apriori (Groceries, parameter = list(supp = 0.002, conf = 0.8))
+# Crearemso 3 variaciones:
 
-rules3 <- apriori (Groceries,parameter = list(supp = 0.001, conf = 0.6))
+# - supp = 0.001, conf = 0.8 la estandar
+# - Aumentaremso legeramente el soporte a 0.002
+# - Disminuiremos legeramente la confianza de 0.8 a 0.6
+
+rules <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.8))
+
+rules2 <- apriori(Groceries, parameter = list(supp = 0.002, conf = 0.8))
+
+rules3 <- apriori(Groceries,parameter = list(supp = 0.001, conf = 0.6))
 
 rules
+# 410 Reglas
 
 rules2
+# 11 Reglas
+
 
 rules3
+# 2918 Reglas
+
+## Depurar un poco a 'rules'
+
+# Pro ahora no dedicaremos a eliminar las reglas repetidas
+
+subset.matrix <- is.subset(rules, rules)
+subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
+Redundantes <- colSums(subset.matrix, na.rm=T) >= 1
+rules.podadas <- rules[!Redundantes]
+rules <- rules.podadas
+
+
 
 ## Organizar por confianza de manera decreciente
 
@@ -101,11 +140,37 @@ yogurt <- subset(rules, subset = rhs %pin% "yogurt")
 
 # Ordenar por confianza
 
-yogurt<-sort(yogurt, by="confidence", decreasing=TRUE)
+yogurt <- sort(yogurt, by="confidence", decreasing=TRUE)
 
 # Inspecionar el top 10
 
 inspect(yogurt[1:5])
+
+
+# Otra forma. desde la aplicacion de apriori
+
+OtherVegetables <- apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.08), 
+                   appearance = list(default="lhs",rhs="other vegetables"),
+                   control = list(verbose=F))
+
+OtherVegetables <- sort(OtherVegetables, decreasing=TRUE,by="confidence")
+
+inspect(OtherVegetables[1:5])
+
+
+
+## Datos que contengan un concecuente espesifico
+
+WholeMilk <- apriori(data=Groceries, parameter=list(supp=0.001,conf = 0.15,minlen=2), 
+               appearance = list(default="rhs",lhs="whole milk"),
+               control = list(verbose=F))
+
+WholeMilk <- sort(WholeMilk, decreasing=TRUE,by="confidence")
+
+inspect(WholeMilk[1:5])
+
+
+### Otros Graficos
 
 plot(rules, control=list(jitter=2, col = rev(brewer.pal(9, "Greens")[4:9])),
      shading = "lift")  
@@ -115,13 +180,20 @@ plot(rules, method="grouped",
 
 plot(rules[1:10],method="graph",interactive=TRUE)
 
-# Borrar redundantes
+plot(rules[1:10],method="graph",interactive=TRUE,shading=NA)
 
-subset <- is.subset(rules, rules)
-subset[lower.tri(subset, diag=T)] <- NA
-Redundantes <- colSums(subset, na.rm=T) >= 1
-Podado <- rules[!Redundantes]
-ReglasPodadas<-Podado
-plot(ReglasPodadas[1:10],method="graph",interactive=TRUE)
+
+plot( head(sort(rules, by="lift"), 10), method="paracoord")
+
+plot( head(sort(rules, by="lift"), 10), method="paracoord", 
+      control=list(reorder=TRUE))
+
+# Grafics Doubledecker  para 1 regla
+
+Regla <- head(sort(rules, by="lift"), 1)
+
+inspect(Regla)
+
+plot(Regla, method="doubledecker", data = Groceries)
 
 
